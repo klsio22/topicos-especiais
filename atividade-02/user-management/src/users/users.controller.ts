589489609576a785b1a3b8e0303cd9f410d75f45
klesio <klesio.nascimento@arrowdigital.group>
2025-09-04 21:50:45 -0300

    feat: implement CRUD operations for user management with in-memory storage

--- atividade-02/user-management/src/users/users.controller.ts
+++ atividade-02/user-management/src/users/users.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Post()
+  create(@Body() body: { name: string }) {
+    return this.usersService.create(body);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() body: { name: string }) {
+    return this.usersService.update(+id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+}
--- atividade-02/user-management/src/users/users.service.ts
+++ atividade-02/user-management/src/users/users.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    { id: 1, name: 'João' },
+    { id: 2, name: 'Maria' }
+  ];
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return this.users.find(user => user.id === id);
+  }
+
+  create(user: { name: string }) {
+    const newUser = {
+      id: this.users.length + 1,
+      ...user,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(id: number, userUpdates: { name: string }) {
+    const userIndex = this.users.findIndex(user => user.id === id);
+    if (userIndex === -1) {
+      return `Usuário com ID ${id} não encontrado`;
+    }
+    this.users[userIndex] = { ...this.users[userIndex], ...userUpdates };
+    return this.users[userIndex];
+  }
+
+  remove(id: number) {
+    const userIndex = this.users.findIndex(user => user.id === id);
+    if (userIndex === -1) {
+      return `Usuário com ID ${id} não encontrado`;
+    }
+    this.users.splice(userIndex, 1);
+    return `Usuário com ID ${id} removido com sucesso`;
+  }
+}
