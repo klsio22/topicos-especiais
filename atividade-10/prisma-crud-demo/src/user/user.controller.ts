9f1d013a95d45bac7bb5a35d023e184d4fb18fd3
klesio <klesio.nascimento@arrowdigital.group>
2025-10-18 19:40:16 -0300

    feat: adicionar módulos e serviços de usuário e prisma ao projeto

---
+++ atividade-10/prisma-crud-demo/src/user/user.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() data: any) {
+    return this.userService.create(data);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(Number(id));
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() data: any) {
+    return this.userService.update(Number(id), data);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(Number(id));
+  }
+}
